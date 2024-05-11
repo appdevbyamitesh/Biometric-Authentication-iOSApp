6489d807a759e38d363f2c1a03dd285cfd387c7b
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2024-05-12 01:00:25 +0530

    [TASK] Biometric Authentication deviceOwnerAuthentication vs deviceOwnerAuthenticationWithBiometrics added

--- BioAuth-iOS/BioAuth-iOS/ViewController.swift
+++ BioAuth-iOS/BioAuth-iOS/ViewController.swift
@@ -18,19 +18,35 @@ class ViewController: UIViewController {
         context.localizedCancelTitle = "Logout App"
         context.localizedFallbackTitle = "Use passcode (2)"
         context.localizedReason = "This app needs your authentication."
+        context.touchIDAuthenticationAllowableReuseDuration = LATouchIDAuthenticationMaximumAllowableReuseDuration
         evaluatePolicy()
     }
     
     func evaluatePolicy() {
         var errorCanEval: NSError?
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &errorCanEval) {
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Fallback title: override reason") { (success, error) in
+        //  deviceOwnerAuthenticationWithBiometrics and deviceOwnerAuthentication are policies used for authenticating the device owner. They are part of the Local Authentication framework, which provides mechanisms for evaluating authentication policies and managing credentials
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &errorCanEval) {
+            switch context.biometryType {
+            case .faceID:
+                debugPrint("face ID")
+            case .touchID:
+                debugPrint("touch ID")
+            case .opticID:
+                debugPrint("optic ID")
+            case .none:
+                debugPrint("None")
+            @unknown default:
+                debugPrint("unknown")
+            }
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Fallback title: override reason") { (success, error) in
                 debugPrint("\(success)")
                 if let err = error {
                     let evalErrCode = LAError(_nsError: err as NSError)
                     switch evalErrCode.code {
                     case LAError.Code.userCancel:
                         debugPrint("User cancelled")
+                    case LAError.Code.appCancel:
+                        debugPrint("App cancelled")
                     case LAError.Code.userFallback:
                         debugPrint("Fallback")
                     case LAError.Code.authenticationFailed:
@@ -43,6 +59,15 @@ class ViewController: UIViewController {
         } else {
             debugPrint("can't evaluate")
             debugPrint(errorCanEval?.localizedDescription ?? "no error desc")
+            if let err = errorCanEval {
+                let evalErrCode = LAError(_nsError: err as NSError)
+                switch evalErrCode.code {
+                case LAError.Code.biometryNotAvailable:
+                    debugPrint("Not enrolled")
+                default:
+                    debugPrint("Other Error")
+                }
+            }
         }
         
     }
